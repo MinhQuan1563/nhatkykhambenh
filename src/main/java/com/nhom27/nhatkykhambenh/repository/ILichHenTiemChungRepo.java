0e40429445312b85df85b633ea6ed4a9ed54842d
quocsi014 <quocsi014@gmail.com>
2024-11-21 09:06:28 +0700

    feature nhac nho tiem chung

--- src/main/java/com/nhom27/nhatkykhambenh/repository/ILichHenTiemChungRepo.java
+++ src/main/java/com/nhom27/nhatkykhambenh/repository/ILichHenTiemChungRepo.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ILichHenTiemChungRepo extends JpaRepository<LichHenTiemChung, Integer> {
     @Query("SELECT l FROM LichHenTiemChung l LEFT JOIN FETCH l.nguoiDungTiemChungList WHERE l.maLichHenTiemChung = :id")
     Optional<LichHenTiemChung> findByIdWithNguoiDungTiemChungList(@Param("id") Integer id);
+
+    List<LichHenTiemChung> findByNgayHenTiemBetween(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/com/nhom27/nhatkykhambenh/repository/INguoiDungTiemChung.java
+++ src/main/java/com/nhom27/nhatkykhambenh/repository/INguoiDungTiemChung.java
@@ -3,10 +3,15 @@ package com.nhom27.nhatkykhambenh.repository;
 import com.nhom27.nhatkykhambenh.model.LichHenTiemChung;
 import com.nhom27.nhatkykhambenh.model.NguoiDungTiemChung;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Set;
 
 public interface INguoiDungTiemChung extends JpaRepository<NguoiDungTiemChung, Integer> {
-    public Set<NguoiDungTiemChung> findAllByLichHenTiemChung(LichHenTiemChung lichHenTiemChung);
+    @Query("SELECT ndtc FROM NguoiDungTiemChung ndtc JOIN FETCH ndtc.nguoiDung WHERE ndtc.lichHenTiemChung = :lichHenTiemChung")
+    List<NguoiDungTiemChung> findAllByLichHenTiemChung(@Param("lichHenTiemChung") LichHenTiemChung lichHenTiemChung);
+
     void deleteByLichHenTiemChung(LichHenTiemChung lichHenTiemChung);
 }
