caf547d10aed32d9bde04eee731f2d613598af0f
quocsi014 <quocsi014@gmail.com>
2024-12-01 18:05:34 +0700

    feat nhac nho kham benh

--- src/main/java/com/nhom27/nhatkykhambenh/repository/ILichHenKhamBenhRepo.java
+++ src/main/java/com/nhom27/nhatkykhambenh/repository/ILichHenKhamBenhRepo.java
@@ -6,11 +6,13 @@ import com.nhom27.nhatkykhambenh.model.NguoiDung;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ILichHenKhamBenhRepo extends JpaRepository<LichHenKham, Long> {
     public LichHenKham findLichHenKhamByKhamBenh(KhamBenh khamBenh);
     @Query("SELECT lh FROM LichHenKham lh WHERE lh.khamBenh.nguoiDung = :nguoiDung ORDER BY lh.thoiGianHen DESC")
     List<LichHenKham> findLichHenKhamByNguoiDung(NguoiDung nguoiDung);
+    public List<LichHenKham> findByThoiGianHenBetween(LocalDateTime start, LocalDateTime end);
 
 }
