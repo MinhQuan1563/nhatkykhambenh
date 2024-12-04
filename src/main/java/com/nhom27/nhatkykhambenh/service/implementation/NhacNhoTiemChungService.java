4bded32b7c82423338c9f4c695ee7cfc2dd234f2
quocsi014 <quocsi014@gmail.com>
2024-12-04 21:11:32 +0700

    fix something

--- src/main/java/com/nhom27/nhatkykhambenh/service/implementation/NhacNhoTiemChungService.java
+++ src/main/java/com/nhom27/nhatkykhambenh/service/implementation/NhacNhoTiemChungService.java
@@ -29,7 +29,7 @@ public class NhacNhoTiemChungService {
     @Autowired
     INguoiDungTiemChung nguoiDungTiemChungRepo;
 
-    @Scheduled(cron = "0 06 9 * * ?")
+    @Scheduled(cron = "0 03 20 * * ?")
     public void sendVaccinationRemider() {
         LocalDateTime startOfTomorrow = LocalDateTime.now().plusDays(1).with(LocalTime.MIN);
         LocalDateTime endOfTomorrow = LocalDateTime.now().plusDays(1).with(LocalTime.MAX);
